256252315fd707936f42afc8a7c4848bffa858ef
John Metta <john@johnmetta.com>
2012-01-11 09:44:41 -0800

    Add images and minor updates

--- functions.js
+++ functions.js
@@ -1,28 +1,73 @@
+/*******************************************************************************
+ * Settings that are somewhat user manageable
+ */
+ 
+// Initial map point that google maps shows in the center of the map
+var center = new google.maps.LatLng(45.589970, -121.188389)
+
+// Initial Zoom Level
+var zoom = 17;
+
+// Delay for each marker to drop (they don't all drop at the same time)
+var timeout = 300;
+
+// The image used for the markers
+var marker_image = 'images/marker.png';
+
+// Size of the video image (This is changed in iphone/android browers, see way below)
+var videoSize = ['299', '182'];
+
+// Returns the text of the stop number
+function stopNumberText(number) {
+    // Change this line to change the string used
+    var text = 'Stop #' + number.toString();
+    return text;
+}
+
+// The HTML for actually formatting the pop-up box. CSS is in map.css
+function generateContent(details) {
+    var content = '<div id="popup">' +
+      '<div id="header">' +
+      '<div id="logo"><img src="images/logo.jpg"></div>' +
+      '<div id="title"><h1 class="all-caps">'+ details.name + '</h1><h2>Location: ' + details.location + '</h2></div>' +
+      '</div>'+ //end header
+      '<div id="videoContent"><iframe width="'+ videoSize[0] + '" height="' + videoSize[1] + '" src="http://www.youtube.com/embed/'+ details.video + '" frameborder="0" allowfullscreen></iframe></div>' +
+      '<div id="bodyContent"><p>' + details.text + '</p><h3><a href="http://www.renewableenergycareers.org">www.renewableenergycareers.org</a></h3>' +
+      '</div>' + //end bodyContent
+      '</div>';
+    return content;
+}
+
+/**************************************************************
+ * Shouldn't have to mess about down here
+ */
 var markers = [];
 var iterator = 0;
 var map;
-var videoSize = ['299', '182'];
 var infowindow = new google.maps.InfoWindow();
+
 /**
  * Initializes the map and listeners.
  */
 function initialize() {
   map = new google.maps.Map(document.getElementById("map_canvas"), {
-        center: new google.maps.LatLng(45.589972, -121.188389),
-        zoom: 17,
+        center: center,
+        zoom: zoom,
         mapTypeId: google.maps.MapTypeId.HYBRID
       });
   drop();
-}     
+}   
 
+// Drop functionality delay
 function drop() {
   for (var i = 0; i < buildings.length; i++) {
     setTimeout(function() {
       addMarker();
-    }, i * 200);
-  }
+    }, i * timeout);
+  }e
 }
 
+// Build up markers and add them to the markers array
 function addMarker() {
   var details = buildings[iterator];
   var marker = new google.maps.Marker({
@@ -32,10 +77,13 @@ function addMarker() {
       map: map,
       draggable: false,
       clickable: true,
-      animation: google.maps.Animation.DROP
+      icon: marker_image,
+      title: stopNumberText((iterator+1).toString()),
+      animation: google.maps.Animation.DROP,
+      zIndex: iterator * -1
   });
   markers.push(marker);
-  var content = "";
+  var content = generateContent(details);
   google.maps.event.addListener(marker, 'click', function () {
      infowindow.content = content;
      infowindow.open(map, marker); 
@@ -43,6 +91,7 @@ function addMarker() {
   iterator++;
 }
 
+// Some visual goodness for mobile apps
 function detectBrowser() {
   var useragent = navigator.userAgent;
   var mapdiv = document.getElementById("map_canvas");
@@ -57,3 +106,5 @@ function detectBrowser() {
     videoSize[1] = '131';
   }
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
